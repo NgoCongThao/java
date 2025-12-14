6cd120fc376b0ba4e3d0fa1f825fe5661daf02e6
CongThao <ngocongthao1511@gmail.com>
2025-12-15 01:56:41 +0700

    Khoi tao du an: Backend va Frontend

---
+++ backend/backend/src/test/java/com/s2o/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2o.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
