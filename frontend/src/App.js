591cc95bc73cb0e270bdad9eee42f4127853a5ed
CongThao <ngocongthao1511@gmail.com>
2025-12-15 02:31:29 +0700

    cau truc du an

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Dự án S2O - Smart Restaurant</h1>
+      <p>Môi trường Frontend đã sẵn sàng!</p>
+      <ul>
+        <li>Dev 1 làm việc tại: /src/pages/guest</li>
+        <li>Dev 3 làm việc tại: /src/pages/staff</li>
+        <li>Dev 5 làm việc tại: /src/pages/admin</li>
+        <li>Dev 7 làm việc tại: /src/pages/kitchen</li>
+      </ul>
     </div>
   );
 }
