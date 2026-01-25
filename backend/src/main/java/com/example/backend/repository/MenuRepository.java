433feb2ff4a7a19fa8c9b346e22d135827de7bd9
MT6060 <MinhTuan.ltp.a7@gmail.com>
2026-01-25 20:17:25 +0700

    Xoa src loi

--- backend/src/main/java/com/example/backend/repository/MenuRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-    // KHÔNG CẦN GÌ THÊM
-}
--- backend/src/main/java/com/example/backend/repository/TenantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Tenant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TenantRepository extends JpaRepository<Tenant, Long> {
-}
